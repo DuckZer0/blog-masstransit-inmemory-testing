a3a111bf70d49ad7ba56d1c69a5eb4a9d099cc5b
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-11-21 13:58:24 +0000

    Add PublishTest (copied from SendTest)

---
+++ MassTransitInMemoryTestingExample.Tests/PublishTest.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net.Config;
+using MassTransit;
+using MassTransit.Log4NetIntegration;
+using NUnit.Framework;
+
+namespace MassTransitInMemoryTestingExample.Tests
+{
+    [TestFixture]
+    public class PublishTest
+    {
+        private const string QueueName = "myQueue";
+        private const string ErrorQueueName = "myQueue_error";
+        private IBusControl _busControl;
+        private Consumer<MyEvent> _fakeEventConsumer;
+        private Consumer<Fault<MyEvent>> _fakeEventFaultConsumer;
+        private readonly ManualResetEvent _manualResetEvent = new ManualResetEvent(false);
+        private ConsumerRegistrar _consumerRegistrar;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeEventConsumer = new Consumer<MyEvent>(_manualResetEvent);
+            _fakeEventFaultConsumer = new Consumer<Fault<MyEvent>>(_manualResetEvent);
+            _consumerRegistrar = CreateSystemUnderTest();
+            ConfigureLog4Net();
+            CreateBus();
+            _busControl.Start();
+        }
+
+        private ConsumerRegistrar CreateSystemUnderTest()
+        {
+            return new ConsumerRegistrar(
+                QueueName,
+                new[] { typeof(Consumer<MyEvent>) },
+                CreateConsumer());
+        }
+
+        private Func<Type, IConsumer> CreateConsumer()
+        {
+            // Very simple in this test as we've only got one type of consumer. In your production code, this func would probably use
+            // an IoC container to resolve the consumer type.
+            return consumerType => _fakeEventConsumer;
+        }
+
+        private void ConfigureLog4Net()
+        {
+            XmlConfigurator.Configure(new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log4net.config")));
+        }
+
+        private void CreateBus()
+        {
+            _busControl = Bus.Factory.CreateUsingInMemory(ConfigureBus);
+        }
+
+        private void ConfigureBus(IInMemoryBusFactoryConfigurator inMemoryBusFactoryConfigurator)
+        {
+            inMemoryBusFactoryConfigurator.UseLog4Net();
+            ConfigureReceiveEndpoints(inMemoryBusFactoryConfigurator);
+        }
+
+        private void ConfigureReceiveEndpoints(IInMemoryBusFactoryConfigurator inMemoryBusFactoryConfigurator)
+        {
+            inMemoryBusFactoryConfigurator.ReceiveEndpoint(_consumerRegistrar.QueueName, _consumerRegistrar.ConfigureEndpoint);
+
+            ConfigureReceiveEndpointToListenForFaults(inMemoryBusFactoryConfigurator);
+        }
+
+        private void ConfigureReceiveEndpointToListenForFaults(IInMemoryBusFactoryConfigurator inMemoryBusFactoryConfigurator)
+        {
+            inMemoryBusFactoryConfigurator.ReceiveEndpoint(ErrorQueueName,
+                receiveEndpointConfigurator =>
+                {
+                    receiveEndpointConfigurator.Consumer(typeof(Consumer<Fault<MyEvent>>),
+                        type => _fakeEventFaultConsumer);
+                });
+        }
+
+        [Test]
+        public async Task Registers_all_consumers_listed_in_consumerTypeProvider_with_the_queue_from_the_receiveEndpointConfiguration()
+        {
+            await PublishFakeEvent();
+            WaitUntilBusHasProcessedMessageOrTimedOut(_manualResetEvent);
+
+            Assert.That(_fakeEventConsumer.ReceivedMessage, Is.True);
+            Assert.That(_fakeEventFaultConsumer.ReceivedMessage, Is.False);
+        }
+
+        private async Task PublishFakeEvent()
+        {
+            await _busControl.Publish(new MyEvent());
+        }
+
+        private void WaitUntilBusHasProcessedMessageOrTimedOut(ManualResetEvent manualResetEvent)
+        {
+            manualResetEvent.WaitOne(TimeSpan.FromSeconds(5));
+        }
+    }
+}
