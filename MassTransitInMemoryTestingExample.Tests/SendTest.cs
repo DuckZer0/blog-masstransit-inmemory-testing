6ea74ca484f0d55abe7964e3c04a831af1af9df4
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-11-21 00:43:22 +0000

    Add project and test project (adapted from my CQRSTutorial repo)

---
+++ MassTransitInMemoryTestingExample.Tests/SendTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using MassTransit;
+using MassTransit.Log4NetIntegration;
+using NUnit.Framework;
+
+namespace MassTransitInMemoryTestingExample.Tests
+{
+    [TestFixture]
+    public class SendTest
+    {
+        private const string QueueName = "myQueue";
+        private const string ErrorQueueName = "myQueue_error";
+        private const string LoopbackAddress = "loopback://localhost/";
+        private IBusControl _busControl;
+        private Consumer<FakeCommand> _fakeCommandConsumer;
+        private Consumer<Fault<FakeCommand>> _fakeCommandFaultConsumer;
+        private readonly ManualResetEvent _manualResetEvent = new ManualResetEvent(false);
+        private ConsumerRegistrar _consumerRegistrar;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fakeCommandConsumer = new Consumer<FakeCommand>(_manualResetEvent);
+            _fakeCommandFaultConsumer = new Consumer<Fault<FakeCommand>>(_manualResetEvent);
+            _consumerRegistrar = new ConsumerRegistrar(
+                QueueName, 
+                new[] { typeof(Consumer<FakeCommand>) }, 
+                CreateConsumer());
+            ConfigureLog4Net();
+            CreateBus();
+            _busControl.Start();
+        }
+
+        private Func<Type, IConsumer> CreateConsumer()
+        {
+            // Very simple in this test as we've only got one type of consumer. In your production code, this func would probably use
+            // an IoC container to resolve the consumer type.
+            return type => _fakeCommandConsumer;
+        }
+
+        private void ConfigureLog4Net()
+        {
+            var loggerRepository = LogManager.GetRepository(Assembly.GetExecutingAssembly());
+            XmlConfigurator.Configure(loggerRepository, new FileInfo("log4net.config"));
+        }
+
+        private void CreateBus()
+        {
+            _busControl = Bus.Factory.CreateUsingInMemory(ConfigureBus);
+        }
+
+        private void ConfigureBus(IInMemoryBusFactoryConfigurator inMemoryBusFactoryConfigurator)
+        {
+            inMemoryBusFactoryConfigurator.UseLog4Net();
+            ConfigureReceiveEndpoints(inMemoryBusFactoryConfigurator);
+        }
+
+        private void ConfigureReceiveEndpoints(IInMemoryBusFactoryConfigurator inMemoryBusFactoryConfigurator)
+        {
+            inMemoryBusFactoryConfigurator.ReceiveEndpoint(_consumerRegistrar.QueueName, _consumerRegistrar.ConfigureEndpoint);
+
+            ConfigureReceiveEndpointToListenForFaults(inMemoryBusFactoryConfigurator);
+        }
+
+        private void ConfigureReceiveEndpointToListenForFaults(IInMemoryBusFactoryConfigurator inMemoryBusFactoryConfigurator)
+        {
+            inMemoryBusFactoryConfigurator.ReceiveEndpoint(ErrorQueueName,
+                receiveEndpointConfigurator =>
+                {
+                    receiveEndpointConfigurator.Consumer(typeof(Consumer<Fault<FakeCommand>>),
+                        type => _fakeCommandFaultConsumer);
+                });
+        }
+
+        [Test]
+        public async Task Registers_all_consumers_listed_in_consumerTypeProvider_with_the_queue_from_the_receiveEndpointConfiguration()
+        {
+            await SendFakeCommand();
+            WaitUntilBusHasProcessedMessageOrTimedOut(_manualResetEvent);
+
+            Assert.That(_fakeCommandConsumer.ReceivedMessage, Is.True);
+            Assert.That(_fakeCommandFaultConsumer.ReceivedMessage, Is.False);
+        }
+
+        private async Task SendFakeCommand()
+        {
+            var sendEndpoint = await GetSendEndpoint();
+            await sendEndpoint.Send(new FakeCommand());
+        }
+
+        private async Task<ISendEndpoint> GetSendEndpoint()
+        {
+            return await _busControl.GetSendEndpoint(new Uri($"{LoopbackAddress}{QueueName}"));
+        }
+
+        private void WaitUntilBusHasProcessedMessageOrTimedOut(ManualResetEvent manualResetEvent)
+        {
+            manualResetEvent.WaitOne(TimeSpan.FromSeconds(5));
+        }
+    }
+}
---
+++ MassTransitInMemoryTestingExample/ConsumerRegistrar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MassTransit;
+
+namespace MassTransitInMemoryTestingExample
+{
+    public class ConsumerRegistrar
+    {
+        private readonly Type[] _consumerTypes;
+        private readonly Func<Type, IConsumer> _createConsumer;
+
+        public ConsumerRegistrar(string queueName, Type[] consumerTypes, Func<Type, IConsumer> createConsumer)
+        {
+            QueueName = queueName;
+            _consumerTypes = consumerTypes;
+            _createConsumer = createConsumer;
+        }
+
+        public string QueueName { get; set; }
+
+        public void ConfigureEndpoint(IReceiveEndpointConfigurator receiveEndpointConfigurator)
+        {
+            foreach (var consumerType in _consumerTypes)
+            {
+                receiveEndpointConfigurator.Consumer(consumerType, _createConsumer);
+            }
+        }
+    }
+}
