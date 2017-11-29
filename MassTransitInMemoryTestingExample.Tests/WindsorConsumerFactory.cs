c03fdef4bd0e67068bd16cbfac22ed4b40b6e13e
Ronan Moriarty <ronan.moriarty@firsthelpline.com>
2017-11-29 15:25:11 +0000

    Add a windsor consumer factory for illustration purposes

---
+++ MassTransitInMemoryTestingExample.Tests/WindsorConsumerFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Castle.Windsor;
+
+namespace MassTransitInMemoryTestingExample.Tests
+{
+    public class WindsorConsumerFactory : IConsumerFactory
+    {
+        private readonly IWindsorContainer _windsorContainer;
+
+        public WindsorConsumerFactory(IWindsorContainer windsorContainer)
+        {
+            _windsorContainer = windsorContainer;
+        }
+
+        public object Create(Type typeToCreate)
+        {
+            return _windsorContainer.Resolve(typeToCreate);
+        }
+    }
+}
